a01900428072b93c0f8393097c78c79bd8765c2d
ArvikJara <arvic2514@gmail.com>
2024-07-03 14:54:58 -0500

    correcion para mostras video y pdf

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -3,20 +3,19 @@
 @section('content')
 <div class="container">
     <h1>{{ $curso->nombre }}</h1>
-    <p><strong>Descripción:</strong> {{ $curso->descripcion }}</p>
-    <p><strong>Categoría:</strong> {{ $curso->categoria }}</p>
+    <p>{{ $curso->descripcion }}</p>
+
     @if ($curso->materiales)
-        <p><strong>Materiales:</strong> <a href="{{ asset('storage/' . $curso->materiales) }}">Descargar PDF</a></p>
+        <a href="{{ asset('storage/' . $curso->materiales) }}" class="btn btn-primary">Descargar Materiales</a>
     @endif
-    <p><strong>Conocimientos Previos:</strong> {{ $curso->conocimientos_previos }}</p>
-    <p><strong>Herramientas:</strong> {{ $curso->herramientas }}</p>
+
     @if ($curso->videos)
-        <p><strong>Videos:</strong></p>
-        <video width="320" height="240" controls>
-            <source src="{{ asset('storage/' . $curso->videos) }}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
+        <div>
+            <video width="600" controls>
+                <source src="{{ asset('storage/' . $curso->videos) }}" type="video/mp4">
+                Tu navegador no soporta la reproducción de videos.
+            </video>
+        </div>
     @endif
-    <a href="/cursos" class="btn btn-secondary">Volver a la lista</a>
 </div>
 @endsection
